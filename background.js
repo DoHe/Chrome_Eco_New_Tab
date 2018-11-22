b4b092875542f0067cff9bd4ac13c6425e869d65
Memes11 <memes@chezmemes.com>
2018-11-22 17:55:43 +0800

    v0.1.21
    Add the option to choose the most-viewed and/or bookmarks. settings pad added

--- background.js
+++ background.js
@@ -1,5 +1,11 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function(data) {
-      console.log("The color is ." + data);
-    });
+	var settings={"mostViewed":1 ,"bookmarks":1 };
+    chrome.storage.sync.set(settings);
+
   });
+
+chrome.storage.onChanged.addListener(function(obj, area){
+	console.log(obj);
+	console.log(area);
+
+});
--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-	
+var settings;/*={"mostViewed":0 ,"bookmarks":0 };*/
 
+
+
+chrome.storage.sync.get(["mostViewed","bookmarks"],function(obj){
+	settings=obj;
+	console.log(settings);
+});
+/*chrome.storage.sync.get("bookmarks",function(obj){
+	settings["bookmarks"]=obj["bookmarks"];
+	console.log(settings);
+});
+
+chrome.storage.sync.get("",function(obj){
+	console.log(obj);
+});
+
+console.log(settings);*/
 
 	$(document).ready(function(){
 		
 		displayBookmarks("1");
+		displayTopSites();
 
 		$( "#bookmarks-bar" ).delegate( ".bookmark-folder", "click", function(){
 			console.log($(this).attr('data-idnode'));
@@ -12,12 +29,45 @@
 
 
 
-	    $("#btn_home").click(function(){
-			chrome.tabs.create({url: "https://www.ecosia.org"});
+	    $("#btn_settings").click(function(){
+			$("#settings").toggle();
 		});
 
-	});
 
+	    $("#settings .switch").each(function() {
+		    $(this).html("<span></span>");
+		    var attr=$(this).attr("dataset");
+		    if(settings[attr]==1){
+		    	console.log(attr);
+		    	$(this).addClass("switch-on");
+		    	$("#" + attr + "-bar").css('display','flex');
+		    }
+		});
+
+
+	    $(".setting .switch").click(function(){
+	    	$(this).toggleClass( "switch-on" );
+
+	    	var attr=$(this).attr("dataset");
+	    	settings[attr] = 1 - settings[attr];
+	    	chrome.storage.sync.set(settings,function(){
+	    		console.log("saved");
+	    	});
+	    	
+	    	/*var display = $( "#" + attr + "-bar" ).css("display");
+    		if ( display === true ) {
+			  $( "#" + attr + "-bar" ).css("display","flex");
+			} else if ( display === false ) {
+			  $( "#" + attr + "-bar" ).css("display","none");
+			}*/
+	    	
+	    	$( "#" + attr + "-bar" ).toggle();
+	    	
+	    });
+
+
+
+		
 	
 
 
@@ -31,7 +81,7 @@
 	});
 
 
-	
+	});
 
 
 function displayBookmarks(idNode){
@@ -40,32 +90,41 @@ function displayBookmarks(idNode){
 		chrome.bookmarks.get(idNode, function(BookmarksTree){
 				console.log(BookmarksTree[0]['parentId']);
 				$( "#bookmarks-bar" ).append( "<p class=\"bookmark-folder\" data-idnode=\""+BookmarksTree[0]['parentId']+"\"><img src=\"img/folder.png\" title=\"..\" alt=\"..\" /><br />..</p>" );
-		});
-		
+		});	
 	}
 	chrome.bookmarks.getChildren(idNode, function(BookmarksTree){
-		/* console.log(BookmarksTree);*/
 		BookmarksTree.forEach(function(element) {
-		  console.log(element['title']);
 		  if (element['title'].length > 16){
-
 		  	var title = element['title'].substring(0,12) + "...";
 		  } else {
 		  	var title = element['title'];
 		  }
-
-
 		  if (element['url']){
 		  	$( "#bookmarks-bar" ).append( "<p class=\"link\"><a href=\"" + element['url']+"\"><img src=\"https://api.faviconkit.com/"+element['url'].split("/")[2]+"/144\" alt=\""+element['url'].split("/")[2]+"\" title=\""+element['title']+"\" /><br />" + title + "</a></p>" );
 		  } else {
 		  	$( "#bookmarks-bar" ).append( "<p class=\"bookmark-folder\" data-idnode=\""+element['id']+"\"><img src=\"img/folder.png\" title=\""+ element['title'] +"\" alt=\""+ element['title'] +"\" /><br />"+ title +"</p>" );
-		  }
-		  
+		  }  
 		});
 	});
-
-
 }
+
+
+function displayTopSites(){
+	$("#mostViewed-bar").html("");
+	chrome.topSites.get(function(topSites){
+		
+		topSites.forEach(function(element, index){
+			console.log(element);
+			if (element['title'].length > 16){
+			  	var title = element['title'].substring(0,12) + "...";
+			} else {
+			  	var title = element['title'];
+			}
+			$( "#mostViewed-bar" ).append( "<p class=\"link\"><a href=\"" + element['url']+"\"><img src=\"https://api.faviconkit.com/"+element['url'].split("/")[2]+"/144\" alt=\""+element['url'].split("/")[2]+"\" title=\""+element['title']+"\" /><br />" + title + "</a></p>" );
+		});
+	})
+}
+
 
 
 
@@ -76,11 +135,6 @@ function displayBookmarks(idNode){
 	});
 
 
-	chrome.bookmarks.getChildren("0", function(BookmarksTree){
-		console.log(BookmarksTree);
-		BookmarksTree.forEach(function(element) {
-		  console.log(element['title']);
-		});
-	});
+	
 
 	
